6ca0cddacfac513fd190df7b72c015847522bdc9
Leonardo Caceres <leomoca19@gmail.com>
2026-04-16 13:15:33 -0400

    merged  profiles

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,33 @@
+Set-variable -Name Global:SOURCE -value "C:/src/" -Option ReadOnly -Force
+Set-Variable -Name Global:CLOUD -Value "$HOME/My Drive/" -Option ReadOnly -Force
+
+Clear-Host
+
+$dev = $SOURCE
 function restart {
-	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 	Write-Host "Path refreshed" -ForegroundColor Cyan
 }
+
+function dev {
+	param ([string]$extrapath = "")
+	Set-Location "$SOURCE$extrapath"
+}
+function sandbox {
+	Set-Location "$SOURCE\sandbox"
+}
+
+function nvim_set {
+	nvim C:\Users\leomo\Appdata\Local\nvim\init.lua
+}
+
+function cloud {
+	param ([string]$extrapath = "")
+	Set-Location "$CLOUD$extrapath"
+}
+
+function gitacp {
+	param ([string]$msg)
+	git add .; git commit -m $msg; git push
+}
+
